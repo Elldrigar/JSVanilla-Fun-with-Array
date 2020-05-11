f0ae8ef3ca8d67fd2de8ff2038d9ef367b037273
Artur Gawron <artur@gawron.me>
2020-05-11 22:55:41 +0200

    Assign btn's to variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMlnBTN = document.getElementById('show-mln');
+const sortBtn = document.getElementById('sort');
+const calculateBTN = document.getElementById('calculate-all');
